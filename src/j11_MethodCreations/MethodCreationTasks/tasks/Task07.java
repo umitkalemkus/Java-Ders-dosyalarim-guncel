5c02877266455f0417e4ecbfea3a143742dac98b
umitkalemkus <umitkalemkus@yahoo.com>
2022-10-05 14:38:26 +0100

    "10.04.2022"

--- src/j11_MethodCreations/MethodCreationTasks/tasks/Task07.java
+++ src/j11_MethodCreations/MethodCreationTasks/tasks/Task07.java
@@ -15,7 +15,7 @@ public class Task07 {
     Beklenen Çıktı:
     12
     */
-       Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Birinci sayiyi giriniz");
         int sayi1 = sc.nextInt();
 
@@ -27,18 +27,27 @@ public class Task07 {
         System.out.println(" Ucuncu sayiyi giriniz");
         int sayi3 = sc.nextInt();
 
-
-
-
-
-
-
-
-
+         kucuksayibulma(sayi1,sayi2,sayi3);
 
 
     }
 
+    public static void kucuksayibulma(int sayi1 , int sayi2 , int sayi3) {
+      if (sayi1 > sayi2 && sayi2 > sayi3){
+          System.out.println("en kucuk sayi = " + sayi3);
+      }else if (sayi3 > sayi1 && sayi2 > sayi1){
+          System.out.println("en kucuk sayi = " + sayi1);
+
+      } else if (sayi2 > sayi1 && sayi1 > sayi3) {
+          System.out.println("en kucuk sayi = " + sayi2);
+
+      }else {
+          System.out.println("Yanlis bir sayi girdiniz");
+
+      }
 
     }
+
+
+}
 
