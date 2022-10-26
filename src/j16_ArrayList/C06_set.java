e6ba5423c801124993426ac050815b7efd184459
umitkalemkus <umitkalemkus@yahoo.com>
2022-10-26 15:05:12 +0100

    26.10.2022

--- src/j16_ArrayList/C06_set.java
+++ src/j16_ArrayList/C06_set.java
@@ -2,16 +2,75 @@ package j16_ArrayList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class C06_set {
 
     public static void main(String[] args) {
-    /// set()--> listin indexe istenen elemani update eder
-        ArrayList<String> listSehir = new ArrayList<>(List.of("Münih", "losAngeles", "Londra", "stockholm"));
-        listSehir.set(3,"Angara");
-        System.out.println(listSehir);
-        //listSehir.set(11,"pataGonya");// olmayan index set edilirse run time alirsinzi
-        System.out.println(listSehir);
+        Scanner klavye=new Scanner(System.in);
+
+        System.out.println("Verilecek Olan Eğitimde Kaç Adet Ders Almak İstiyorsunuz  ?");
+
+        int n=klavye.nextInt();
+
+
+
+        ArrayList<String> dersler =new ArrayList<>();
+
+
+
+        System.out.println("Almak İstediğiniz Dersleri Yazınız...");
+
+
+
+        for(int i=0;i<n;i++){
+
+            dersler.add(klavye.next());
+
+        }
+
+        System.out.println("Aldığınız Dersler :");
+
+        for(int k=0; k<n; k++){
+
+            System.out.println(dersler.get(k));
+
+        }
+
+        System.out.println();
+
+
+
+        System.out.println("Eklediğiniz Derslerden Hangisinin Sırasını Öğrenmek İstiyorsunuz?");
+
+
+
+        String ara=klavye.next();
+
+        System.out.println();
+
+
+
+        if(dersler.contains(ara)==true){
+
+
+
+            System.out.println("Eklediğiniz Ders :"+ ara);
+
+
+
+            System.out.println("Eklediğiniz Dersin Eğitimini "+  (dersler.indexOf(ara)+1)  +". Sırada Alacaksınız");
+
+
+
+        }else{
+
+            System.out.println("Aradığınız Ders, Seçimlerinizde Gözükmemektedir.");
+
+
+
+        }
+
 
 
     }
---
+++ src/j21_ImmutableClass/C03_OCA_ImmutableClass.java
@@ -0,0 +1,17 @@
+package j21_ImmutableClass;
+
+public class C03_OCA_ImmutableClass {
+    public static void main(String[] args) {
+        String a = "";
+        a += 2;
+        a += 'c';
+        a += false;
+
+        if (a == "2cfalse") {
+            System.out.println("==");
+        }
+        if (a.equals("2cfalse")) {
+            System.out.println("equals");
+        }
+    }
+}
