65b08dd1e8bf58df11c86bc7cbf613f09c640a34
umitkalemkus <umitkalemkus@yahoo.com>
2022-10-28 19:54:53 +0100

    yeni

---
+++ src/j20_PassByvalue/Task3.java
@@ -0,0 +1,42 @@
+package j20_PassByvalue;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Task3 {
+     /*
+    Task 03 ->
+    Lesson Class :  fields : name, credit (1-10 arasında değer almalı)
+    Student Class : fields : name, maxCredit, dersleri(Lesson class obj) saklayacak list
+                   derslerin toplam kredi miktarını return eden  toplamKredisi method
+    Runner Class :  3 adet  ders ve 1 adet öğrc (maxCredit=10) obj create ederek öğrencinin alabileceği ve
+                       alamayacagı dersleri print eden code create ediniz
+
+     */
+    //fields
+    String name;
+    int credit;
+    int maxCredit;
+
+
+    public static void LessonClass(String name, int credit){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i <list.size() ; i++) {
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
