7eb7e36d706c4a0dadf20478258fee807368f582
umitkalemkus <umitkalemkus@yahoo.com>
2023-04-20 19:45:35 +0100

    done

--- src/day4/Q02_StringManupulation.java
+++ src/day4/Q02_StringManupulation.java
@@ -6,6 +6,9 @@ public class Q02_StringManupulation {
     //Scanner kullanarak iki ayri deger giriniz ve bu iki kelimeyi method kullanarak birlestiriniz.
     //yukardaki ornekte verilen ilk ve ikinci degiskenlerinin ilk harflerini atip birlestiriniz.
     public static void main(String[] args) {
+
+
+
         Scanner scan = new Scanner(System.in);
         System.out.println("String 1 :");
         String str1 = scan.nextLine();
