f495e0e7adaa99945229870c5b56e259e33819b3
umitkalemkus <umitkalemkus@yahoo.com>
2023-04-30 16:31:02 +0100

    done

--- src/day4/Q04_StringManupulation.java
+++ src/day4/Q04_StringManupulation.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Q04_StringManupulation {
 
 
+
+
+
+
         /*
          * Kulanicidan bir kelime isteyin eger kelime 3 ve daha fazla harfden olusuyorsa son
          * iki harfini 3 kere yan yana yazdirin. degil ise girilen kelimeyi yazdirin
