dc0c7c8fa47aabfbefb21e900f0d3e682a068b86
umitkalemkus <umitkalemkus@yahoo.com>
2023-12-26 23:55:19 +0000

    20.11.23

--- src/EmailCreation/main.java
+++ src/EmailCreation/main.java
@@ -4,6 +4,7 @@ public class main {
 
 
     public static void main(String[] args) {
+
         EmailApp emailApp = new EmailApp("Garry","F");
         //  System.out.println(emailApp.setDepartment());
         System.out.println(emailApp.showInfo());
